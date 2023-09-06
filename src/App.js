e6e609a4eb4d08dde8a62d70ef24e51727cf5db6
Albert Company <albertdc1984@gmail.com>
2023-09-06 18:02:03 +0200

    Created items state and passit to child components as props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import Form from "./components/Form";
 import Logo from "./components/Logo";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const handleAddItems = (item) => {
+    setItems((items) => [...items, item]);
+  };
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -1,15 +1,10 @@
 import Item from "./Item";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 2, packed: true },
-];
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
